075f26c043045574162eead4903aaabed1b01584
Charlie Steele <steelech@umich.edu>
2016-10-20 20:44:02 -0400

    Create users controller and route

--- app/components/signup-form.js
+++ app/components/signup-form.js
@@ -5,6 +5,7 @@ export default Ember.Component.extend({
 	ajax: Ember.inject.service(),
 	actions: {
 		register: function() {
+			// need to modify this to check if the password matches the confirmation before sending it
 			let { identification, password, confirmation } = this.getProperties('identification', 'password', 'confirmation');
 			return this.get('ajax').request('/users', {
 				method: 'POST',
