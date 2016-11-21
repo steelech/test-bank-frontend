af68af651475448abfe39579a79567cb7ea0b5a5
Charlie Steele <steelech@umich.edu>
2016-11-20 20:20:33 -0500

    Better styling for navbar dropdown

--- app/components/nav-dropdown.js
+++ app/components/nav-dropdown.js
@@ -1,5 +1,16 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+	session: Ember.inject.service(),
 	tagName: 'li',
+	user: '',
+	init() {
+		this._super(...arguments);
+
+	},
+	actions: {
+		logout() {
+			this.get('session').invalidate();
+		}
+	}
 });
