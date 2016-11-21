2a8113469db4caf42b23db7260f4773d5606796d
Charlie Steele <steelech@umich.edu>
2016-11-20 22:58:40 -0500

    styling changes (background/button colors) and cleaner uploads list

--- app/controllers/uploads.js
+++ app/controllers/uploads.js
@@ -1,12 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+	formOpen: false,
 	actions: {
-		imageUploadComplete: function(details) {
-			let fullUrl = details.fullUrl, key = details.key;
-			console.log("fullUrl:", fullUrl);
-			console.log("key:", key);
-			alert("Upload complete!");
+		closeUploadForm() {
+			this.set("formOpen", false);
 		},
+		openUploadForm() {
+			this.set("formOpen", true);
+		}
 	}
 });
--- app/routes/uploads.js
+++ app/routes/uploads.js
@@ -2,4 +2,18 @@ import Ember from 'ember';
 import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 
 export default Ember.Route.extend(AuthenticatedRouteMixin, {
+	formOpen: false,
+	model: function(params) {
+		return this.get('store').findAll("upload");
+
+	},
+	actions: {
+		closeUploadForm() {
+			this.set("formOpen", false);
+		},
+		openUploadForm() {
+			this.set("formOpen", true);
+			console.log("hi");
+		}
+	}
 });
