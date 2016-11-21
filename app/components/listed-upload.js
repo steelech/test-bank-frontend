d50da323c20b296a2e51063c9d43fc0e568d9c1f
Charlie Steele <steelech@umich.edu>
2016-11-20 23:59:44 -0500

    styling for listed-upload component

--- app/components/listed-upload.js
+++ app/components/listed-upload.js
@@ -1,4 +1,23 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+	uploadModal: false,
+	iconClass: 'hidden',
+	tagName: 'li',
+	classNames: ['list-group-item', 'listed-upload'],
+	over: false,
+	actions: {
+		over() {
+			this.set("over", true);
+			this.set("iconClass", "listed-upload-icon");
+		},
+		off() {
+			this.set("over", false);
+			this.set("iconClass", "hidden");
+		},
+		viewUpload() {
+			console.log("viewing upload");
+
+		}
+	}
 });
