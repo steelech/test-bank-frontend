c81573da50eab3554927d1334028b6ffa0270bbd
Charlie Steele <steelech@umich.edu>
2016-11-21 23:28:33 -0500

    start styling for active dashboard tab

--- app/routes/dashboard.js
+++ app/routes/dashboard.js
@@ -18,22 +18,52 @@ export default Ember.Route.extend(AuthenticatedRouteMixin, {
 			this.get('session').invalidate();
 		},
 		setFilterType(type) {
-			if(type == 'all') {
-				this.setAllActive();
-			} else if(type == 'mine') {
-				this.setMineActive();
-			} else {
-				this.setByCourseActive();
-			}
+
+			//if(type == 'all') {
+				//this.setAllActive();
+			//} else if(type == 'mine') {
+				//this.setMineActive();
+			//} else {
+				//this.setByCourseActive();
+			//}
+			this.removeSelected(type);
 		}
 	},
+	setFilterStyling(type) {
+		
+		// we want to try and set the styling of the selected tab and the unselected tabs all in one function without the code being too ugly
+		// we need to base our actions off of 'type'
+		//
+			
+	},
 	setAllActive() {
-		console.log("setting 'All' as the active tab");
+		$('.uploads-all-filter').attr('id', 'uploads-navbar-text-selected');
+		$('.uploads-mine-filter').attr('id', 'uploads-navbar-text-unselected');
+		$('.uploads-course-filter').attr('id', "uploads-navbar-text-unselected");
 	},
 	setMineActive() {
-		console.log("setting 'My Uploads' as the active tab");
+		$('.uploads-all-filter').attr('id', 'uploads-navbar-text-unselected');
+		$('.uploads-mine-filter').attr('id', 'uploads-navbar-text-selected');
+		$('.uploads-course-filter').attr('id', "uploads-navbar-text-unselected");
 	},
 	setByCourseActive() {
-		console.log("setting 'By Course' as the active tab");
+		$('.uploads-all-filter').attr('id', 'uploads-navbar-text-unselected');
+		$('.uploads-mine-filter').attr('id', 'uploads-navbar-text-unselected');
+		$('.uploads-course-filter').attr('id', "uploads-navbar-text-selected");
+	},
+	removeSelected(selected) {
+		var options = ['all', 'mine', 'course'];
+		var array = [];
+		var len = options.length;
+		for (var i=0;i<len;i++) {
+			if(options[i] != selected) {
+				array.push(options[i]);
+			}
+		}
+		array.push('selected');
+		$('uploads' + array[0] + 'filter').attr('id', 'uploads-navbar-text-unselected');
+		$('uploads' + array[1] + 'filter').attr('id', 'uploads-navbar-text-unselected');
+		$('uploads', + array[2] + 'filter').attr('id', 'uploads-navbar-text-selected');
+
 	}
 });
