9fd73e0e8e8945ba8dd41d1ce5c5a2a949d97538
Charlie Steele <steelech@umich.edu>
2016-11-28 12:14:23 -0500

    add model for courses

--- app/components/course-filter.js
+++ app/components/course-filter.js
@@ -1,4 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+	courses: [],
+       	store: Ember.inject.service(),
+	init() {
+		this._super(...arguments);
+		this.set("courses", this.get("store").findAll("course"));
+	}
 });
