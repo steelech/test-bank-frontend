4991671a2ad9058c8908a49ad08d56997444a3af
Charlie Steele <steelech@umich.edu>
2016-11-27 22:17:34 -0500

    finish moving dashboard navbar logic from route to controller

--- app/controllers/dashboard.js
+++ app/controllers/dashboard.js
@@ -8,11 +8,9 @@ export default Ember.Controller.extend({
 	actions: {
 
 		openNewUploadModal() {
-			console.log("opening modal in controller");
 			this.set("showNewUploadModal", true);
 		},
 		closeNewUploadModal() {
-			console.log("closing modal in controller");
 			this.set("showNewUploadModal", false);
 		},
 		
--- app/routes/dashboard.js
+++ app/routes/dashboard.js
@@ -6,10 +6,6 @@ const { service } = Ember.inject;
 
 export default Ember.Route.extend(AuthenticatedRouteMixin, {
 	session: Ember.inject.service('session'),
-	filter: 'all',
-	init() {
-		this.set("filter", "all");
-	},
 	
 	model: function(params) {
 		return this.get('store').findAll("upload");
@@ -20,47 +16,5 @@ export default Ember.Route.extend(AuthenticatedRouteMixin, {
 		logout() {
 			this.get('session').invalidate();
 		},
-		setFilterType(type) {
-			var self = this;
-			this.set("filter", type);
-			this.removeSelected(type, self).then(function(array) {
-				self.styleTabs(array);
-			});
-		}
 	},
-	setFilterStyling(type) {
-		
-		// we want to try and set the styling of the selected tab and the unselected tabs all in one function without the code being too ugly
-		// we need to base our actions off of 'type'
-		//
-			
-	},
-	removeSelected(selected, self) {
-		var array = ['all', 'mine', 'course']
-		var finalArray = [];
-		var self = this;
-		var promise = new Promise(function(resolve, reject) {
-			for(var i = 0;i < array.length;i++) {
-				if(selected != array[i]) {
-					finalArray.push(array[i]);
-				}
-			}
-			finalArray.push(selected);
-			resolve({array: finalArray});
-		});
-		return promise;
-
-	},
-	styleTabs(newTabs) {
-		// the first two are unselected, the third is selected
-		var prefix = '.uploads-';
-		var suffix = '-filter';
-		var tabs = newTabs.array;
-		for(var i = 0;i < tabs.length - 1;i++) {
-			var selector = prefix + tabs[i] + suffix;
-			$(selector).attr("id", "uploads-navbar-text-unselected");
-		}
-		var selector = prefix + tabs[tabs.length - 1] + suffix;
-		$(selector).attr("id","uploads-navbar-text-selected");
-	}
 });
