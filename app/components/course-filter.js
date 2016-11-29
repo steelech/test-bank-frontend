afef030ea697c71e41503ee2fda385d9ecb27d25
Charlie Steele <steelech@umich.edu>
2016-11-28 22:48:08 -0500

    Get courses from server when course-filter is rendered

--- app/components/course-filter.js
+++ app/components/course-filter.js
@@ -1,10 +1,19 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-	courses: [],
        	store: Ember.inject.service(),
-	init() {
-		this._super(...arguments);
-		this.set("courses", this.get("store").findAll("course"));
+        courses: null,
+        didInsertElement() {
+		var self = this;
+		var myCourses = Ember.A([]);
+		this.get("store").findAll("course").then((courses) => {
+			var coursesArray = courses.toArray();
+			var i;
+			for(i = 0;i < coursesArray.length;i++) {
+				console.log(coursesArray[i].data.name);
+				myCourses.push(coursesArray[i].data.name);
+			}
+			self.set("courses", myCourses);
+		});
 	}
 });
