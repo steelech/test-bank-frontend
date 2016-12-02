43885c3abc76f69bdde0095ee19e20027323333e
Charlie Steele <steelech@umich.edu>
2016-12-02 14:08:57 -0500

    add file browse button to file upload

--- app/components/file-upload.js
+++ app/components/file-upload.js
@@ -4,8 +4,14 @@ import $ from 'npm:jquery';
 export default Ember.Component.extend({
 	classNames: ['file-upload'],
 	didInsertElement() {
+		var self = this;
 		console.log("loaded page");
 		var dragAndDrop = $('#draganddrophandler');
+		var fileBrowse = $("#file-browse");
+
+		fileBrowse.on('change', function(e) {
+			console.log("file picked!");
+		});
 		dragAndDrop.on('dragenter', function(e) {
 			e.stopPropagation();
 			e.preventDefault();
@@ -21,7 +27,14 @@ export default Ember.Component.extend({
 		dragAndDrop.on('drop', function(e) {
 			e.preventDefault();
 			console.log("dropped!");
+			var files = e.originalEvent.dataTransfer.files;
+			self.handleFileUpload(files);
+
 
 		});
+	},
+	handleFileUpload(files) {
+		console.log("files: ", files);
 	}
+
 });
